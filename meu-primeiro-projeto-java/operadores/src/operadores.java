2e068aeaa98ef35200c16ea4844d1e7f679d0275
VivianaAndrade <vivibecker.andrade@gmail.com>
2022-12-18 22:42:18 -0300

    operadores_aula_2

---
+++ meu-primeiro-projeto-java/operadores/src/operadores.java
@@ -0,0 +1,24 @@
+public class operadores {
+    
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM"+" "+"JAVA";
+        //concatenação
+
+        System.out.println (nomeCompleto);
+         
+        //qual o resultado das expressoes abaixo?
+
+String concatenacao ="?"; 
+
+concatenacao = 1+1+1+"1";
+
+concatenacao = 1+"1"+1+1;
+
+concatenacao = 1+"1"+1+"1";
+
+concatenacao = "1"+1+1+1;
+
+concatenacao = "1"+(1+1+1);
+
+    }
+}

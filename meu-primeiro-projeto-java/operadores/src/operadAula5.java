c2809fce03de483ab2fbe822cde99282783fbdd1
VivianaAndrade <vivibecker.andrade@gmail.com>
2022-12-19 13:56:41 -0300

    operadores ternários

---
+++ meu-primeiro-projeto-java/operadores/src/operadAula5.java
@@ -0,0 +1,33 @@
+public class operadAula5 {
+    
+    public static void main(String[] args){
+// classe Operadores.java
+    int a, b;
+    a = 5;
+    b = 6;
+    
+    String resultado = "";
+
+// EXEMPLO DE CONDICIONAL //UTILIZANDO UMA ESTRUTURA IF/ELSE
+if(a==b)
+   resultado = "verdadeiro";
+else
+   resultado = "falso";
+
+   System.out.println(resultado);
+
+   //opção2:
+
+   /*MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO*/
+
+   int c, d;
+        c = 6;
+        d = 6;
+        String resultado2 = c==d ? "verdadeiro" : "falso";
+
+        System.out.println(resultado2);
+
+    }
+}
+
+    

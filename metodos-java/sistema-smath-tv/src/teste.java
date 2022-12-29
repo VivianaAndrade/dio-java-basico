e1d4d88afc3ebfbacb8f4c212a4bb381b7473ee7
VivianaAndrade <vivibecker.andrade@gmail.com>
2022-12-29 02:04:27 -0300

    mediaIdade

---
+++ metodos-java/sistema-smath-tv/src/teste.java
@@ -0,0 +1,9 @@
+public class teste {
+    enum cor
+    { RED , GREEN , BLUE };
+
+public static void main (String []args)
+{           cor corEscolhida = cor.RED;
+            System.out.println("Cor"+corEscolhida);
+    }
+}
--- meu-primeiro-projeto-java/desafio-media-idade/src/mediaIdade.java
+++ meu-primeiro-projeto-java/desafio-media-idade/src/mediaIdade.java
@@ -2,25 +2,21 @@ import java.util.Scanner;
 
 public class mediaIdade {
     public static void main(String[] args) {
+        
         Scanner idade = new Scanner(System.in);
 
         for(int i=0; i<=2; i++){
         System.out.println("Informe a sua idade:");
         int idade1 = idade.nextInt();
 
-        }
-
-        if (idade  >=0 && idade <26){
+        if (idade1 >=0 && idade1 <26){
             System.out.println ("Jovem");
         }
-        else if (idade >=26 && idade <=60){
+        else if (idade1 >25 && idade1 <61){
             System.out.println ("Adulto");
         }
         else 
         System.out.println("Idoso");
-    
-        for(int i=0; i<=2; i++){
-            System.out.println("Sua idade:"+idade1);
         }
     }
 }
